960ae97fac79d12b54b0676a1523516a6e2af0d9
Avinash Chitare <chitareavinash6@gmail.com>
2024-07-14 02:10:03 +0530

    built search function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,28 @@
 import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../utils/NavbarSlice";
+import { useState, useEffect } from "react";
+import { YOUTUBE_SEARCH_KEY } from "../utils/Constants";
 
 const Header = () => {
-  const dispatch = useDispatch();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestion, setShowSuggestion] = useState(false);
 
+  useEffect(() => {
+    const Timer = setTimeout(() => searchResults(), 200);
+    return () => {
+      clearTimeout(Timer);
+    };
+  }, [searchQuery]);
+
+  const searchResults = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_KEY + searchQuery);
+    const json = await data.json();
+    setSuggestions(json[1]);
+    console.log(searchQuery);
+  };
+
+  const dispatch = useDispatch();
   const toggleAction = () => {
     dispatch(toggleMenu());
   };
@@ -22,14 +41,32 @@ const Header = () => {
           src="https://t3.ftcdn.net/jpg/05/07/46/84/360_F_507468479_HfrpT7CIoYTBZSGRQi7RcWgo98wo3vb7.jpg"
         />
       </div>
-      <div className="col-span-10 px-10 ">
+      <div className=" col-span-10 px-10 ">
         <input
           type="text"
-          className="w-1/2  p-1 border border-gray rounded-l-3xl bg-gray"
+          className=" w-1/2 p-1 border border-s-4 rounded-l-3xl bg-gray-100"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onFocus={() => setShowSuggestion(true)}
+          onBlur={() => setShowSuggestion(false)}
         />
         <button className=" p-1 border border-gray rounded-r-3xl bg-gray">
           🔍
         </button>
+        {showSuggestion && (
+          <div className="fixed px-1 bg-white w-[450px]  rounded-md">
+            <ul>
+              {suggestions.map((suggestion) => (
+                <li
+                  key={suggestion}
+                  className=" px-2 py-2 rounded-xl hover:bg-gray-200"
+                >
+                  🔍 {suggestion}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
       <div className="col-span-1">
         <img
--- src/utils/Constants.jsx
+++ src/utils/Constants.jsx
@@ -2,3 +2,5 @@ const API_KEY = "AIzaSyCf2rfSdKnidLUZDC9c34F36PbrHY6jEVM";
 export const YOUTUBE_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=80&regionCode=IN&key=" +
   API_KEY;
+export const YOUTUBE_SEARCH_KEY =
+  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
